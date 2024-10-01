5771ec5812aa0171f15f58ea5d37c806a9e5b8df
Frangnzlz <151971568+Frangnzlz@users.noreply.github.com>
2024-10-01 12:39:02 +0200

    finalizacion EjConYBuc

---
+++ Ejercicios_CondicionalesyBubles/Ejercicio7.js
@@ -0,0 +1,23 @@
+var num1 = parseInt(prompt("Introduce el primer numero", ""));
+var num2 = 0;
+while (num1 > num2) {
+  var num2 = parseInt(prompt("Introduce el segundo numero", ""));
+}
+
+var numPrimos = "";
+
+for (i = num1; i <= num2; i++) {
+  if (esPrimo(i)) {
+    numPrimos += `${i} `;
+  }
+}
+alert(numPrimos);
+
+function esPrimo(num) {
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}

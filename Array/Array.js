cef44e1c1c1bd04652c5e04eade93c698b1c20a3
Frangnzlz <151971568+Frangnzlz@users.noreply.github.com>
2024-10-08 11:55:33 +0200

    FinArrays

--- Array/Array.js
+++ Array/Array.js
@@ -1,7 +1,13 @@
-var arra = new Array(5);
+var arra = new Array();
 var cont = 0;
 
 
+function reiniciar(){
+    document.getElementById("intr").value = "";
+    arra = new Array();
+    cont = 0
+    document.getElementById("num").innerText = `${cont+1}`;
+}
 
 function relArray() {
     let num = parseInt(document.getElementById("intr").value);
@@ -28,18 +34,51 @@ function calSuma(){
             sum += arra[i];
         }
         document.getElementById("result").innerText = `La suma de los ${cont} numeros del array es ${sum} `;
-        arra = new Array(5);
-        cont = 0
-        document.getElementById("num").innerText = `${cont+1}`;
+
     }
+    document.getElementById("intr").value = "";
+
 }
 
 function mayor(){
     arra.sort();
     document.getElementById("result").innerText = `El mayor numero dentro del array es ${arra[arra.length-1]}`;
 
+    document.getElementById("intr").value = "";
+
+}
+function mostrarPar(){
+
+    document.getElementById("result").innerText = esPar()
+    ? 'Todos los elementos son pares'
+    : 'No todos los elementos son pares';
+    document.getElementById("intr").value = "";
+
 }
 
+
 function esPar(){
-    
+    for(let i = 0; i < arra.length; i++){
+        if(arra[i] % 2 != 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+
+function contar(){
+    let cont = 0;
+    let busc = parseInt(document.getElementById("intr").value);
+
+    for(let i = 0; i < arra.length; i++){
+        if(arra[i] == busc){
+            cont++;
+        }
+    }
+    document.getElementById("result").innerText = cont === 0
+    ? 'No se ha encontrado ese numero'
+    : `Ese numero se ha encontrado ${cont} veces en el array`;
+    document.getElementById("intr").value = "";
+
 }

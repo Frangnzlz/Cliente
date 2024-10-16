57529127a9f80765b25f8df4b1849739bf4b1e93
Frangnzlz <151971568+Frangnzlz@users.noreply.github.com>
2024-10-16 13:19:45 +0200

    8-9

--- Array/Array_8-10.js
+++ Array/Array_8-10.js
@@ -22,3 +22,22 @@ function sumaParcial(array){
     return resultArray;
 
 }
+
+function ej9(){
+    console.log(countBy());
+}
+
+function countBy(){
+    let num = parseInt(prompt("Introduce el numero del que quieras saber los multiplos"));
+    let tam = parseInt(prompt("Introduce el numero de multiplos que quieras saber"));
+    let array = [];
+    if(num<0 || tam<1){
+        return array;
+    }
+
+    for(let i = 0; i < tam;i++){
+        array.push((i+1)*num);
+    }
+
+    return array;
+}

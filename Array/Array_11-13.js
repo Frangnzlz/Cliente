1b8d075b7469282f973acdce71554ec71b24956d
Frangnzlz <151971568+Frangnzlz@users.noreply.github.com>
2024-10-17 14:39:37 +0200

    ej12

--- Array/Array_11-13.js
+++ Array/Array_11-13.js
@@ -2,8 +2,6 @@ function ej11(){
     let frase = prompt("Introduce una frase")
     let array =[];
 
-    console.log(frase.split(" "))
-
     array = frase.split(" ");
 
     array.reverse();
@@ -12,4 +10,60 @@ function ej11(){
 
     listar(array, ", ");
 }
+
+function ej12(){
+    let array1 = ["Ray","Andres", "Jose", "Dani"];
+    let array2 = ["Miguel", "Dani", "Jose", "Ivan"];
+
+    console.log(buscarDistintos(array1,array2))
+}
+function buscarDistintos(primerArray, segundoArray){
+    var array1 = [];
+    var array2 = [];
+    let i = 0;
+    
+    while(i < primerArray.length || i < segundoArray.length){
+
+        if(i < primerArray.length){
+            if(!(buscarElemento(primerArray[i], segundoArray))){
+                array1.push(primerArray[i]);
+            }
+        }
+        if(i < segundoArray.length){
+            if(!(buscarElemento(segundoArray[i], primerArray))){
+                array2.push(segundoArray[i]);
+            }
+        }
+        i++;
+    }
+
+
+    // for(let i = 0; i < primerArray.length; i++){
+    //     if(!(buscarElemento(primerArray[i], segundoArray))){
+    //         array1.push(primerArray[i]);
+    //     }
+
+    // }
+
+    // for(let i = 0; i < segundoArray.length; i++){
+    //     if(!(buscarElemento(segundoArray[i], primerArray))){
+    //         array1.push(segundoArray[i]);
+    //     }
+
+    // }
+
+    return array1.concat(array2);
+}
+
+function buscarElemento(elemento, array){
+    for(let i = 0; i< array.length;i++){
+        if(elemento == array[i]){
+            return true
+        }
+    }
+    return false;
+}
+
+
+
 

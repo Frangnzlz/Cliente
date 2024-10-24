57ac27bd7cf21e76e1949709c00874c5b9d8035b
Frangnzlz <151971568+Frangnzlz@users.noreply.github.com>
2024-10-24 14:17:01 +0200

    xd

--- Array/Avanzados_2/Array_18-21.js
+++ Array/Avanzados_2/Array_18-21.js
@@ -84,13 +84,12 @@ function likes(array){
         case 0:
             return `Nobody likes this`;
         case 1:
-            return `${array[0]} likes this`
+            return `${array[0]} likes this`;
         case 2:
-            return `${array[0]} and ${array[1]} like this`    
+            return `${array[0]} and ${array[1]} like this`;    
         case 3:
-            return `${array[0]}, ${array[1]} and ${array[2]} like this`    
-    
+            return `${array[0]}, ${array[1]} and ${array[2]} like this`;   
         default:
-            return `${array[0]}, ${array[1]} and ${array.length-2} like this`    
+            return `${array[0]}, ${array[1]} and ${array.length-2} like this`;    
     }
 }
